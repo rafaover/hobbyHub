b0413cedcce7307b5b95c3c7ff2bc49b53ebec0c
rafaover <rafamsilva@gmail.com>
2023-08-09 13:47:40 +1000

    MainActivity.kt - Moved TopicCard component to /component - Created method TopicGridList to show the list with proper logic.

--- app/src/main/java/com/feeltheboard/hobbyhub/MainActivity.kt
+++ app/src/main/java/com/feeltheboard/hobbyhub/MainActivity.kt
@@ -3,26 +3,20 @@ package com.feeltheboard.hobbyhub
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Card
-import androidx.compose.material3.Icon
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.feeltheboard.hobbyhub.model.Topic
+import com.feeltheboard.hobbyhub.components.TopicCard
+import com.feeltheboard.hobbyhub.data.DataSource.topics
 import com.feeltheboard.hobbyhub.ui.theme.HobbyHubTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //
+                    TopicGridList()
                 }
             }
         }
@@ -43,39 +37,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Row {
-            Image(
-                painter = painterResource(topic.image),
-                contentDescription = stringResource(topic.title),
-                modifier.size(68.dp),
-                contentScale = ContentScale.Crop
-            )
-            Column (modifier
-                .padding(start = 16.dp, top = 16.dp, end = 16.dp)
-            ) {
-                Text(
-                    text = stringResource(topic.title),
-                    style = MaterialTheme.typography.bodyMedium,
-                    modifier = Modifier.padding(bottom = 8.dp)
-
-                )
-                Row (verticalAlignment = Alignment.CenterVertically){
-                    Icon(painter = painterResource(R.drawable.ic_grain), contentDescription = null)
-                    Text(
-                        text = topic.quantity.toString(),
-                        style = MaterialTheme.typography.labelMedium,
-                        modifier = Modifier.padding(start = 8.dp)
-                    )
-                }
-            }
+fun TopicGridList(modifier: Modifier = Modifier) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(8.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        modifier = modifier
+    ) {
+//        topics.forEach { topic ->
+//            item {
+//                TopicCard(topic)
+//            }
+//        }
+        items(topics) { topic ->
+            TopicCard(topic)
         }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-private fun CourseGrid() {
-    TopicCard(Topic(R.string.architecture, 23, R.drawable.architecture))
+fun DefaultPreview() {
+    HobbyHubTheme {
+        TopicGridList()
+    }
 }
