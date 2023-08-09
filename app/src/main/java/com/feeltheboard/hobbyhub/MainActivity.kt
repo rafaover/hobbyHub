1bb735b1e2c4fe2711f708622164aa677687344e
rafaover <rafamsilva@gmail.com>
2023-08-09 12:23:00 +1000

    MainActivity.kt: - Finished TopicCard composable.

--- app/src/main/java/com/feeltheboard/hobbyhub/MainActivity.kt
+++ app/src/main/java/com/feeltheboard/hobbyhub/MainActivity.kt
@@ -7,16 +7,21 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.feeltheboard.hobbyhub.model.Topic
 import com.feeltheboard.hobbyhub.ui.theme.HobbyHubTheme
 
@@ -38,15 +43,31 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun topicCard(topic: Topic, modifier: Modifier = Modifier) {
+fun TopicCard(topic: Topic, modifier: Modifier = Modifier) {
     Card(modifier = modifier) {
         Row {
-            Image(painter = painterResource(topic.image), contentDescription = stringResource(topic.title))
-            Column {
-                Text(text = stringResource(topic.title))
-                Row {
+            Image(
+                painter = painterResource(topic.image),
+                contentDescription = stringResource(topic.title),
+                modifier.size(68.dp),
+                contentScale = ContentScale.Crop
+            )
+            Column (modifier
+                .padding(start = 16.dp, top = 16.dp, end = 16.dp)
+            ) {
+                Text(
+                    text = stringResource(topic.title),
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(bottom = 8.dp)
+
+                )
+                Row (verticalAlignment = Alignment.CenterVertically){
                     Icon(painter = painterResource(R.drawable.ic_grain), contentDescription = null)
-                    Text(text = stringResource(topic.quantity))
+                    Text(
+                        text = topic.quantity.toString(),
+                        style = MaterialTheme.typography.labelMedium,
+                        modifier = Modifier.padding(start = 8.dp)
+                    )
                 }
             }
         }
@@ -55,6 +76,6 @@ fun topicCard(topic: Topic, modifier: Modifier = Modifier) {
 
 @Preview(showBackground = true)
 @Composable
-fun courseGrid() {
-    topicCard()
+private fun CourseGrid() {
+    TopicCard(Topic(R.string.architecture, 23, R.drawable.architecture))
 }
